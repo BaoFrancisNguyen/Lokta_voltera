9de58b75aa77626ea11fed9fe49dd4c3e8d87236
api-scraping <bao.francis.nguyen@gmail.com>
2025-01-06 11:02:51 +0100

    move function on a separated file

--- lotka_voltera_app.py
+++ lotka_voltera_app.py
@@ -2,24 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-# Fonction pour charger les données réelles depuis un fichier CSV
-def load_csv_data(file_path):
-    data = pd.read_csv(file_path)
-    return data['Time'], data['Prey'], data['Predator']
-
-# Fonction pour calculer la MSE
-# on utilise le carré de la différence entre les valeurs réelles et les valeurs simulées pour obtenir une valeur positive
-
-def calculate_mse(real_prey, real_predator, simulated_prey, simulated_predator):
-    # on calcule la moyenne des carrés des différences
-    mse_prey = np.mean((real_prey - simulated_prey) ** 2)
-    mse_predator = np.mean((real_predator - simulated_predator) ** 2)
-    
-    return mse_prey, mse_predator
-
-# Charger un fichier CSV
-csv_file_path = input("populations_lapins_renards.csv")
-time_real, prey_real, predator_real = load_csv_data(csv_file_path)
 
 # valeurs proportionnelles à la population de proies et de prédateurs
 rabbit = [1]

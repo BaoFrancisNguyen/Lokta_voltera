71c95141eaa2a2d5bc906723193326c29239c17f
api-scraping <bao.francis.nguyen@gmail.com>
2025-01-06 10:39:13 +0100

    adding file

---
+++ lotka_voltera_app.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+# valeurs proportionnelles à la population de proies et de prédateurs
+rabbit = [1]
+fox = [2]
+time =[0]
+alpha = 2/3
+beta = 4/3
+gamma = 1
+delta = 1
+step = 0.01
+
+#approximation of the differential equation
+for _ in range(0, 100_000):
+    new_time_value = time[-1]
+    new_rabbit_value = (rabbit[-1] * (alpha - beta * fox[-1])) * step + rabbit[-1]
+    new_fox_value = (fox[-1] * (delta * rabbit[-1] - gamma)) * step + fox[-1]
+
+    rabbit.append(new_rabbit_value)
+    fox.append(new_fox_value)
+    time.append(new_time_value + step)
+
+
+plt.figure(figsize=(15, 6))
+plt.plot(time, rabbit, label='Rabbit', color='blue')
+plt.plot(time, fox, label='Fox', color='red')
+plt.legend(' évolution de la population de proies et de prédateurs')
+plt.show()

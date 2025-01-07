a8220b97fbf55b9d3d4fd9bc44036c9c20fe4143
api-scraping <bao.francis.nguyen@gmail.com>
2025-01-07 19:50:24 +0100

    adding load csv data verification values

--- lotka_voltera_functions.py
+++ lotka_voltera_functions.py
@@ -5,8 +5,15 @@ from lotka_voltera_app import time, rabbit, fox
 
 # Fonction pour charger les données réelles depuis un fichier CSV
 def load_csv_data(file_path):
-    data = pd.read_csv(file_path)
-    return data['Time'], data['Prey'], data['Predator']
+    try:
+        data = pd.read_csv(file_path)
+        # Vérifie que les colonnes nécessaires sont présentes
+        if 'Time' in data.columns and 'Prey' in data.columns and 'Predator' in data.columns:
+            return data['Time'].values, data['Prey'].values, data['Predator'].values
+        else:
+            raise ValueError("Le fichier CSV doit contenir les colonnes 'Time', 'Prey', et 'Predator'.")
+    except Exception as e:
+        raise IOError(f"Erreur lors du chargement du fichier CSV : {e}")
 
 # Fonction pour calculer la MSE
 # on utilise le carré de la différence entre les valeurs réelles et les valeurs simulées pour obtenir une valeur positive
